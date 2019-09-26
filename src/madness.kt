bf0899a068d8b2efbe2c3a98d10b48e30018a441
K1ycee <chiagozieani145@gmail.com>
2019-09-26 20:46:42 +0100

    Initial Kotlin commit

---
+++ src/madness.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    var a = 32
+    var b = 56
+    val pr = keyboard(a,b)
+    print ("The annoying product is $pr")
+}
+fun keyboard (a:Int,b:Int): Int {
+    return a*b
+}
